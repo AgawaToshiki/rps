ed697536c3756afe3ed72813b32a54a4cdf1bfca
AgawaToshiki <t.agawa1128ht@gmail.com>
2023-12-25 19:14:32 +0900

    スタイル微調整

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center max-w-[1920px] min-h-screen">
       <div className="">
-        <h1 className="flex justify-center my-10">うぇぶじゃんけん</h1>
+        <h1 className="flex justify-center mb-10">うぇぶじゃんけん</h1>
         <input 
             type="text" 
             value={ isName } 

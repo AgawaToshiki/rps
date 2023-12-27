68948a205930525e16a50905ff5b387f4caa106b
Agawa <t.agawa1128ht@gmail.com>
2023-12-27 14:48:11 +0900

    グループの影のスタイルを変更

--- app/components/Group.tsx
+++ app/components/Group.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Group = ({ groupName }: { groupName: string }) => {
   return (
-    <div className="px-2 py-10 bg-red-200 shadow-lg text-center font-bold text-xl max-lg:text-lg max-md:text-base max-sm:text-sm max-md:px-0 relative top-0 transition-all duration-100 ease-out hover:-top-[3px] active:top-0 active:shadow-md">
+    <div className="px-2 py-10 bg-red-200 shadow-card text-center font-bold text-xl max-lg:text-lg max-md:text-base max-sm:text-sm max-md:px-0 relative top-0 transition-all duration-100 ease-out hover:-top-[3px] active:top-0 active:shadow-none">
       { groupName }
     </div>
   )

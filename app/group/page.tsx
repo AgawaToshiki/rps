b1fa8d440e6a004ab0b33f0d4baf5f6942ad7b4e
Agawa <t.agawa1128ht@gmail.com>
2023-12-27 14:14:30 +0900

    グループの色を見やすい色に変更

--- app/group/page.tsx
+++ app/group/page.tsx
@@ -240,10 +240,10 @@ const GroupPage = () => {
         </div>
         { getMember.map((member) => (
           <div key={ member.userId } className="flex w-full items-center mb-2">
-            <div className={`${isAllReady && isGameStart === "waiting" ? 'bg-yellow-200': 'bg-red-200'} flex justify-center items-center w-[50%] h-[80px] p-2`} >
+            <div className={`${isAllReady && isGameStart === "waiting" ? 'bg-yellow-100': 'bg-red-200'} flex justify-center items-center w-[50%] h-[80px] p-2`} >
               <p className="text-xl max-lg:text-lg max-md:text-base max-sm:text-sm">{ member.displayName }</p>
             </div>
-            <div className={`${isAllReady && isGameStart === "waiting" ? 'bg-yellow-200': 'bg-red-200'} flex justify-center items-center w-[50%] h-[80px] p-2`}>
+            <div className={`${isAllReady && isGameStart === "waiting" ? 'bg-yellow-100': 'bg-red-200'} flex justify-center items-center w-[50%] h-[80px] p-2`}>
               {isGameStart === "playing"
                 ? (<div>
                   {member.choice

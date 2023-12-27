3980914e008db9b18ed2dbf93e8dc182aefab2ba
Agawa <t.agawa1128ht@gmail.com>
2023-12-27 09:05:47 +0900

    スタイル修正

--- app/components/Group.tsx
+++ app/components/Group.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Group = ({ groupName }: { groupName: string }) => {
   return (
-    <div className="p-10 bg-pink-200 border text-center font-bold">{ groupName }</div>
+    <div className="px-2 py-10 bg-pink-200 border text-center font-bold text-xl max-lg:text-lg max-md:text-base max-sm:text-sm max-md:px-0">{ groupName }</div>
   )
 }
 
